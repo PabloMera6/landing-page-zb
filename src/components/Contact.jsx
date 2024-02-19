7066c67dae06591cd596654266fa85053da7b0c3
Alejandro Campano Galán <72875687+Alejandrocg024@users.noreply.github.com>
2024-02-19 22:18:25 +0100

    v0.5

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-
-import { RiMailFill } from "react-icons/ri";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const retForm = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    emailjs.sendForm('service_2zj5w9e', 'template_4jx7x7p', retForm.current, 'user_6oVqzv4q9z6l6X2rNfZ8I')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
   return (
-    <div id="contact" className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-20 p-8 md:p-12 xl:p-20">
+    <div
+      id="contact"
+      className=" bg-bg"
+    >
+      <div className='className="grid xl:p-20 xl:max-w-[1200px] l:max-w-[800px] md:max-w-[600px] mx-[2vh] md:mx-auto text-primary-200'>
         Contacto
+      </div>
     </div>
   )
 }
