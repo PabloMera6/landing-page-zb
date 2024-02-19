ed5eb3e82883000de4ecda2cfcaf7929f898b3ce
Alejandrocg024 <alejandrocg024@gmail.com>
2024-02-19 09:43:16 +0100

    Home done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,33 @@
-// import Logo from './assets/logo.png'
+import React, { useState } from "react";
+// Icons
+import {
+  RiMenu3Fill,
+  RiCloseLine,
+} from "react-icons/ri";
 
 const Header = () => {
+    const [showMenu, setShowMenu] = useState(false);
     return (
-        <header className="flex items-center w-full p-4 h-[10vh] bg-primary-100">
-            <div className="w-2/6 flex justify-center">
-                <img src="logo.png"className="h-[10vh] object-cover"/>
+        <header className="flex items-center justify-between xl:justify-start w-full p-4 h-[10vh] bg-primary-100">
+            <div className="xl:w-2/6 flex items-center justify-center h-full ml-[2vh]">
+                <img src="logo.png"className="max-h-full max-w-full object-cover"/>
             </div>
-            <nav className="flex-1 justify-center flex items-center gap-14 text-white">
+            <nav
+                className={`fixed bg-primary-100 w-[70%] md:w-[40%] xl:w-full h-full ${showMenu ? "left-0" : "-left-full"
+                    } top-0 xl:static flex-1 flex flex-col xl:flex-row items-center justify-center gap-14 text-white transition-all duration-500 z-50`}
+            >
                 <a href="#" className="relative hover:text-gray-300 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-300 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-300 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">Inicio</a>
                 <a href="#" className="relative hover:text-gray-300 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-300 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-300 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">Producto</a>
                 <a href="#" className="relative hover:text-gray-300 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-300 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-300 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">¿Quiénes somos?</a>
                 <a href="#" className="relative hover:text-gray-300 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-300 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-300 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">Lanzamientos</a>
                 <a href="#" className="relative hover:text-gray-300 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-300 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-300 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">Contacto</a>
             </nav>
+            <button
+                onClick={() => setShowMenu(!showMenu)}
+                className="xl:hidden text-2xl p-2"
+            >
+                {showMenu ? <RiCloseLine /> : <RiMenu3Fill />}
+            </button>
         </header>
     );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,24 +2,26 @@
 
 const Hero = () => {
   return (
-    <section className='bg-bg h-[90vh] grid grid-cols-1 md:grid-cols-10'>
-        <div className="md:col-span-6 flex items-center justify-center text-primary-100">
-            <div className="flex flex-col gap-2 text-center">
-                <h1 className="text-7xl font-bold">Bienvenidos!</h1>
-                <p className="text-4xl">Explora la innovación 3D.</p>
-                <p className="text-4xl">Descubre nuestra comunidad.</p>
-                  <div className="flex flex-col md:flex-row items-center justify-center gap-4">
-                      <a href="#" className="relative flex h-[50px] w-40 rounded-xl items-center justify-center overflow-hidden bg-primary-200 font-medium text-white shadow-2xl transition-all duration-300 before:absolute before:inset-0 before:border-0 before:border-white before:duration-100 before:ease-linear hover:bg-white hover:text-primary-200 hover:shadow-primary-200 hover:before:border-[25px]">
-                          <span className="relative z-10">Descubre más</span>
-                      </a>
-                      <a href="#" className="relative flex h-[50px] w-40 rounded-xl items-center justify-center overflow-hidden bg-primary-200 font-medium text-white shadow-2xl transition-all duration-300 before:absolute before:inset-0 before:border-0 before:border-white before:duration-100 before:ease-linear hover:bg-white hover:text-primary-200 hover:shadow-primary-200 hover:before:border-[25px]">
-                          <span className="relative z-10">Contáctanos</span>
-                      </a>
-                  </div>
+    <section className='bg-bg'>
+        <div className="h-[90vh] grid grid-cols-1 md:grid-cols-10 xl:max-w-[2400px] l:max-w-[1200px] mx-auto">
+            <div className="md:col-span-5 flex items-center justify-center text-primary-100">
+                <div className="flex flex-col gap-2 text-center">
+                    <h1 className="text-5xl xl:text-7xl font-bold">Bienvenidos!</h1>
+                    <p className="text-2xl xl:text-4xl">Explora la innovación 3D.</p>
+                    <p className="text-2xl xl:text-4xl">Descubre nuestra comunidad.</p>
+                    <div className="flex flex-col md:flex-row items-center justify-center gap-4">
+                        <a href="#" className="relative flex h-[50px] w-40 rounded-xl items-center justify-center overflow-hidden bg-primary-200 font-medium text-white shadow-2xl transition-all duration-300 before:absolute before:inset-0 before:border-0 before:border-white before:duration-100 before:ease-linear hover:bg-white hover:text-primary-200 hover:shadow-primary-200 hover:before:border-[25px]">
+                            <span className="relative z-10">Descubre más</span>
+                        </a>
+                        <a href="#" className="relative flex h-[50px] w-40 rounded-xl items-center justify-center overflow-hidden bg-primary-200 font-medium text-white shadow-2xl transition-all duration-300 before:absolute before:inset-0 before:border-0 before:border-white before:duration-100 before:ease-linear hover:bg-white hover:text-primary-200 hover:shadow-primary-200 hover:before:border-[25px]">
+                            <span className="relative z-10">Contáctanos</span>
+                        </a>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div className="md:col-span-4 flex items-center justify-center p-12">
-            <img src="hero.png"className="h-30 object-cover mr-20"/>
+            <div className="md:col-span-5 flex items-center justify-center p-12">
+                    <img src="hero.png"className="h-30 object-cover"/>
+            </div>
         </div>
     </section>
   )
