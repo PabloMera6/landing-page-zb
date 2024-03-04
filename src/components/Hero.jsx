c12138531479f2ab38ad854d68de57b7cf128651
Juan Jesús Campos Garrido <guaje3181@gmail.com>
2024-03-04 23:00:48 +0100

    Quitado clockify

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,11 +17,6 @@ const Hero = () => {
                             <span className="relative z-10">Contáctanos</span>
                         </a>
                     </div>
-                    <div className="flex flex-col md:flex-row items-center justify-center gap-4">
-                        <a href="https://app.clockify.me/shared/65d87eea756abe0bdf308a40" className="relative flex h-[50px] w-40 rounded-xl items-center justify-center overflow-hidden bg-primary-200 font-medium text-white shadow-2xl transition-all duration-300 before:absolute before:inset-0 before:border-0 before:border-white before:duration-100 before:ease-linear hover:bg-white hover:text-primary-200 hover:shadow-primary-200 hover:before:border-[25px]">
-                            <span className="relative z-10">Clockify</span>
-                        </a>
-                    </div>
                 </div>
             </div>
             <div className="md:col-span-5 flex items-center justify-center p-12">
