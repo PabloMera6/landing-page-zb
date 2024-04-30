8a78ccb60af2c015e7e0a02f25781772cb3686a8
Alejandro Campano Galán <72875687+Alejandrocg024@users.noreply.github.com>
2024-04-30 16:52:34 +0200

    redes sociales

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,10 @@
 // import React from 'react'
+import { BsTwitterX } from "react-icons/bs";
+import { FaYoutube } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitch } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -9,6 +15,26 @@ const Hero = () => {
                     <h1 className="text-5xl xl:text-7xl font-bold">Bienvenidos!</h1>
                     <p className="text-2xl xl:text-4xl">Explora la innovación 3D.</p>
                     <p className="text-2xl xl:text-4xl">Descubre nuestra comunidad.</p>
+                    <div className="flex items-center justify-center gap-10 text-2xl xl:text-6xl">
+                        <a href="https://twitter.com/shar3d_2024">
+                            <BsTwitterX className="transition-transform hover:scale-125" />
+                        </a>
+                        <a href="https://www.facebook.com/people/Shar3d/61558534868827/">
+                            <FaFacebook className="transition-transform hover:scale-125" />
+                        </a>
+                        <a href="https://www.twitch.tv/shar3d_2024">
+                            <FaTwitch className="transition-transform hover:scale-125" />
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCA-3C2FB2mZB-jY1V3KGHnQ">
+                            <FaYoutube className="transition-transform hover:scale-125" />
+                        </a>
+                        <a href="https://www.tiktok.com/@shar3d_2024">
+                            <FaTiktok className="transition-transform hover:scale-125" />
+                        </a>
+                        <a href="https://www.instagram.com/shar3d_2024/">
+                            <FaSquareInstagram className="transition-transform hover:scale-125" />
+                        </a>
+                    </div>
                     <div className="flex flex-col md:flex-row items-center justify-center gap-4">
                         <a href="#" className="relative flex h-[50px] w-40 rounded-xl items-center justify-center overflow-hidden bg-primary-200 font-medium text-white shadow-2xl transition-all duration-300 before:absolute before:inset-0 before:border-0 before:border-white before:duration-100 before:ease-linear hover:bg-white hover:text-primary-200 hover:shadow-primary-200 hover:before:border-[25px]">
                             <span className="relative z-10">Descubre más</span>
